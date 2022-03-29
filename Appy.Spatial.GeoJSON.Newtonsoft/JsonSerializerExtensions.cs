b6d302931510d91d93e4ed5cfda4bef756b71d52
Milian <milian.lichere@gmail.com>
2022-03-29 15:08:37 +0100

    perf: avoid converting to string before calling PopulateObject
    Co-authored-by: MilianL <milian.lichere@appyway.com>
    Co-authored-by: Jorge Rodriguez Galan <jrogalan@gmail.com>

--- Appy.Spatial.GeoJSON.Newtonsoft/JsonSerializerExtensions.cs
+++ Appy.Spatial.GeoJSON.Newtonsoft/JsonSerializerExtensions.cs
@@ -1,25 +1,21 @@
-﻿using System.IO;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Appy.Spatial.GeoJSON.Newtonsoft
 {
     public static class JsonSerializerExtensions
     {
-        public static T PopulateObject<T>(this JsonSerializer serializer, string value, T target)
+        public static T PopulateObject<T>(this JsonSerializer serializer, JsonReader jsonReader, T target)
         {
-            using (JsonReader jsonReader = new JsonTextReader(new StringReader(value)))
+            serializer.Populate(jsonReader, target);
+
+            if (!serializer.CheckAdditionalContent)
+                return target;
+
+            while (jsonReader.Read())
             {
-                serializer.Populate(jsonReader, target);
-
-                if (!serializer.CheckAdditionalContent)
-                    return target;
-
-                while (jsonReader.Read())
+                if (jsonReader.TokenType != JsonToken.Comment)
                 {
-                    if (jsonReader.TokenType != JsonToken.Comment)
-                    {
-                        throw new JsonSerializationException("Additional text found in JSON string after finishing deserializing object.");
-                    }
+                    throw new JsonSerializationException("Additional text found in JSON string after finishing deserializing object.");
                 }
             }
             
